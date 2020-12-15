08725c17a362e8e850660c560ec2c36985e76488
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2020-12-15 12:49:52 -0300

    ok

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
+let array = [];
+
 var corpo = document.getElementById('corpo');
 
 for (let i = 0; i < 3; i++) {
     var torre = document.createElement('div');
     torre.setAttribute("id", i);
-    torre.setAttribute("class", "torres")
+    torre.setAttribute("class", "torres");
+
     corpo.appendChild(torre);
+
+    document.getElementById(i).onclick = function() {
+
+        array.push(i);
+
+        if (array.length == 2) {
+
+
+            // tenho um array [x, y] x é de onde vai sair, y é pra onde vai
+
+
+
+            var tabela2 = document.getElementById(array[1]);
+            var disco = document.createElement('div');
+            disco.setAttribute("id", document.getElementById(array[0]).firstElementChild.id);
+            disco.setAttribute("class", "disco")
+            tabela2.appendChild(disco);
+            console.log('vai criar: ' + document.getElementById(array[0]).firstElementChild.id);
+
+
+
+            document.getElementById(array[0]).removeChild(document.getElementById(array[0]).childNodes[0]);
+
+
+
+
+            console.log(array)
+            array.length = 0; // zera o array
+            //criar função para  retirar e colocar a div
+
+
+
+        }
+    }
+
+
 }
 
 var tabela = document.getElementById('0');
@@ -17,6 +56,7 @@ for (let i = 0; i < 4; i++) {
 }
 
 
+
 /// on click na div remove disco 1
 // on click torre 2 ou 3 coloca o disco
 
@@ -24,8 +64,3 @@ for (let i = 0; i < 4; i++) {
 
 //REMOVE CHILD OK
 // tabela.removeChild(tabela.childNodes[0]);
-
-
-
-
-console.log(tabela.firstElementChild);
